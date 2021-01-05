a6a7d3967516666d52e8594bddbd58feb6c77e3b
JavedAjk <ajavedk2021@users.noreply.github.com>
2021-01-05 16:45:35 +0530

    resolved pagination api issue

--- scripts.js
+++ scripts.js
@@ -1,108 +1,114 @@
-var current_page = 1;
-var records_per_page = 2;
-let next='';
-let prev='';
+let nextPageUrl = '';
+let prevPageUrl = '';
 const apiURL = 'https://api.lyrics.ovh';
-const cors= 'https://cors-anywhere.herokuapp.com/';
+const cors = 'https://cors-anywhere.herokuapp.com';
+const headersData = { 'Accept-Charset': 'utf-8', 'Content-Type': 'application/json', 'X-Requested-With': 'xhr' };
 const search = document.getElementById('songsearch')
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit',function(e){
-e.preventDefault();
-console.log(form.elements.songsearch.value);
-searchKeyword = search.value.trim()
 
-if(!searchKeyword){
-    alert("No keywords to search")
-}
-else{ 
-    searchSong(searchKeyword)
-}
+// search song based on kewyword
 
-})
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchKeyword = search.value.trim()
+    if (!searchKeyword) { alert("No keywords to search") }
+    else { searchSong(searchKeyword) }
+});
+
+// search song API call
+
 const searchSong = async (searchKeyword) => {
-try{
-const res = await fetch(`${apiURL}/suggest/${searchKeyword}`);
-const data = await res.json();
-console.log('data: ',data);
-next= data.next;
-console.log('dataNext: ', next);
-showData(data);
-}catch(e){
-console.log('something went wrong',e);
-}
+    try {
+        const res = await fetch(`${apiURL}/suggest/${searchKeyword}`);
+        const data = await res.json();
+        nextPageUrl = data.next;
+        showData(data);
+    } catch (e) {
+        console.log('something went wrong', e);
+    }
 }
 
-function showData(data){
-  
-    result.innerHTML = `
-    <ul class="song-data">
-      ${data.data
-        .map(song=> `<li>
-                    <div>
-                        <strong>${song.artist.name}</strong> -${song.title} 
-                    </div>
-                    <span artist="${song.artist.name}" songtitle="${song.title}"> Show Lyrics</span>
-                </li>`
-        )
-        .join('')}
-    </ul>
-  `;
-}
+// render response data of searched keyword
 
-result.addEventListener('click', e=>{
+function showData(data) {
+    result.innerHTML = `<ul class="song-data">
+                        ${data.data.map(song =>
+                        `<li>
+                            <div>
+                                <strong>${song.artist.name}</strong> -${song.title} 
+                            </div>
+                            <span artist="${song.artist.name}" songtitle="${song.title}"> Show Lyrics</span>
+                        </li>`).join('')
+                        }</ul>`;
+ }
+
+// show lyrics 
+
+result.addEventListener('click', e => {
     const clickedElement = e.target;
-    if (clickedElement.tagName === 'SPAN'){
+    if (clickedElement.tagName === 'SPAN') {
         const artist = clickedElement.getAttribute('artist');
-        const songTitle = clickedElement.getAttribute('songtitle');     
+        const songTitle = clickedElement.getAttribute('songtitle');
         showLyrics(artist, songTitle)
     }
-})
+});
+
+// lyrics api call
 
 async function showLyrics(artist, songTitle) {
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
-    const data = await res.json(); 
+    const data = await res.json();
     const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
-    <p>${lyrics}</p>`;
-  
-  }
-// pagination
-
-
-
-
-async function nextPage()
-{
-    if(!next){
-        alert('No records to show ');
-    }else{
-    try{
-
-        const res = await fetch(cors+`/`+next);
-        const data = await res.json();
-        console.log('data: ',data);
-        prev= data.prev;
-        showData(data);
-        }catch(e){
-        console.log('something went wrong on clicking next',e);
-        }
-    } 
+    result.innerHTML =
+     `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+      <p>${lyrics}</p>`;
 }
 
-async function prevPage()
-{
-    if(!prev){
+// pagination API call on click
+//next page
+async function nextPage() {
+  if (!nextPageUrl) {
+    if (!search.value) { alert("Type a keyword to search") }
+else{
         alert('No records to show ');
-    }else{
-    try{
-        const res = await fetch(cors+`/`+prev);
-        const data = await res.json();
-        console.log('data: ',data);
-        showData(data);
-        }catch(e){
-        console.log('something went wrong on clicking prev',e);
+    }} else {
+        try {            
+            fetch(`${cors}/${nextPageUrl}`, headersData)
+              .then(response => response.text())
+              .then(res => {
+                const jdata = JSON.parse(res);
+                prevPageUrl = jdata.prev;
+                nextPageUrl = jdata.next;
+                showData(jdata);
+              })
+              .catch(error => console.log('error', error));
+        } catch (e) {
+            console.log('something went wrong on clicking next', e);
         }
-}}
+    }
+}
+
+// prev page
+async function prevPage() {
+    if (!prevPageUrl) {
+        if (!search.value) { alert("Type a keyword to search") }
+        else{
+                alert('No records to show ');
+            }
+    } else {
+        try {
+            fetch(`${cors}/${prevPageUrl}`, headersData)
+              .then(response => response.text())
+              .then(res => {
+                const jdata = JSON.parse(res);
+                showData(jdata);
+              })
+              .catch(error => console.log('error', error));
+        } catch (e) {
+            console.log('something went wrong on clicking prev', e);
+        }
+    }
+}
 
 
 exports.searchSong = searchSong;
